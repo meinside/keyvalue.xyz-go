20ad342b698d6f3f5d07efd923bcb9596661dadf
Sungjin Han <meinside@gmail.com>
2018-08-27 21:41:48 +0900

    Re-add SetObjectAndValidate function and edit test

--- keyvalue_test.go
+++ keyvalue_test.go
@@ -36,42 +36,43 @@ func TestKeyValue(t *testing.T) {
 }
 
 func TestKeyValueObj(t *testing.T) {
-	val1 := obj{Name: "Tester1", Age: 37, Occupation: "Software Engineer"}
+	val1 := obj{Name: "Tester1", Age: 37, Occupation: "Developer"}
 	val2 := obj{Name: "Tester2", Age: 39, Occupation: "Entrepreneur"}
 
 	if kv, err := NewKeyValue(KeyForTesting); err == nil {
 		// Set and validate with function
-		if err := kv.SetObjectAndValidateFunc(val1, func(v string, o interface{}) bool {
+		if err := kv.SetObjectAndValidate(val1, func(v string, o interface{}) bool {
 			var r obj // returned object
 			if err := json.Unmarshal([]byte(v), &r); err != nil {
 				t.Error("Failed to unmarshal value:", v, "-", err)
+				return false
 			}
 
 			if r.Name == val1.Name && r.Age == val1.Age && r.Occupation == val1.Occupation {
 				return true
-			} else {
-				t.Error("Unmarshalled object is different from request object:", r, "-", val1)
-				return false
 			}
+
+			t.Error("Unmarshalled object is different from the requested object:", r, "-", val1)
+			return false
 		}); err != nil {
-			t.Error("Failed to set value:", val1, "-", err)
+			t.Error(err)
 		}
 
 		// Get
 		if _, err := kv.Get(); err != nil {
-			t.Error("Failed to get value for key:", kv.Key, "-", err)
+			t.Error(err)
 		}
 
 		// Set again (not validating)
 		if err := kv.SetObject(val2); err != nil {
-			t.Error("Failed to update value to:", val2, "-", err)
+			t.Error(err)
 		}
 
 		// Get
 		if _, err := kv.Get(); err != nil {
-			t.Error("Failed to get updated value for key:", kv.Key, "-", err)
+			t.Error(err)
 		}
 	} else {
-		t.Error("Failed to generate a new key:", err)
+		t.Error(err)
 	}
 }
